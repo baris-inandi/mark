d3e845c9551777b56c0666419903b966a31d3cbd
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-02-15 22:51:29 +0300

    close but not there yet

--- src/classes/dom.py
+++ src/classes/dom.py
@@ -14,7 +14,7 @@ class Dom:
         ]
 
     def to_html(self):
-        out, parents_trace = "", [Node("_null", 0)]
+        out, stack = "", [Node("_null", 0)]
         for index, node in enumerate(self.nodes):
             if node.tag != "_null":
                 # if node.tag == "_plaintext":
@@ -26,18 +26,14 @@ class Dom:
                 # and html plaintext
                 next_node = self.nodes[index + 1]
                 node.parse_attr()
+                out += " " * node.indent + node.opening_tag() + "\n"
                 if node.indent < next_node.indent:
-                    # parent
                     # right tab
-                    out += " " * node.indent + node.opening_tag() + "\n"
-                    parents_trace.append(node)
-                elif node.indent > next_node.indent:
-                    # child
-                    # left tab
-                    parent = parents_trace[-1]
-                    out += " " * parent.indent + parent.closing_tag() + "\n"
-                    parents_trace.pop()
+                    stack.append(node)
                 else:
-                    # sibling
-                    out += " " * node.indent + node.open_close_tag() + "\n"
+                    # left tab
+                    if node.indent > next_node.indent:
+                        stack.pop()
+                    parent = stack[-1]
+                    out += " " * parent.indent + parent.closing_tag() + "\n"
         return out
