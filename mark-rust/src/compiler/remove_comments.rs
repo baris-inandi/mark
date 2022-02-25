ef033cd1a56c3a939c54d9eee0fa52f5fb7911a2
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-02-26 00:36:42 +0300

    commit

--- mark-rust/src/compiler/remove_comments.rs
+++ mark-rust/src/compiler/remove_comments.rs
@@ -1,64 +1,70 @@
-/* pub fn remove_comments(code: String) -> String {
-    let mut new_code = String::new();
-    let mut in_comment = false;
-    let mut in_oneline = false;
-    let mut in_multiline = false;
-    let mut in_double_quote = false;
-    let mut in_single_quote = false;
-    let mut in_block = false;
-    for line in code.lines() {
-        let mut new_line = String::new();
-        for (index, char) in line.chars().enumerate() {
-            if char == '"' && !(in_block && in_single_quote) {
-                in_double_quote = !in_double_quote;
-            } else if char == '\'' && !(in_block && in_double_quote) {
-                in_single_quote = !in_single_quote;
-            } else if char == '`' && !(in_double_quote && in_single_quote) {
-                in_block = !in_block;
-            }
-            if index < line.len() {
-                if char == '/' && !in_comment {
-                    let next_char = line.chars().nth(index + 1).unwrap();
-                    if next_char == '/' {
-                        in_oneline = true;
-                        in_comment = true;
-                    } else if next_char == '*' {
-                        in_multiline = true;
-                        in_comment = true;
-                    }
-                }
-                if in_multiline {
-                    if char == '*' {
-                        let next_char = line.chars().nth(index + 1).unwrap();
-                        if next_char == '/' {
-                            in_multiline = false;
-                            in_comment = false;
-                        }
-                    }
-                }
-            }
-            if in_oneline {
-                if char == '\n' {
-                    in_oneline = false;
-                    in_comment = false;
-                }
-            }
-            if !in_comment {
-                new_line.push(char);
-            }
-        }
-        new_code.push_str(&new_line);
-        new_code.push_str("\n");
-    }
-    return new_code;
+struct MarkStringContext {
+    in_block: bool,  // `string`
+    in_single: bool, // 'string'
+    in_double: bool, // "string"
 }
- */
 
-pub fn remove_comments(source: String) -> String {
-    let mut code = source.to_owned();
-    code.push_str("\n");
-    for (index, c) in code.chars().enumerate() {
-        println!("{} {}", index, c);
+impl MarkStringContext {
+    fn new() -> MarkStringContext {
+        MarkStringContext {
+            in_block: false,
+            in_single: false,
+            in_double: false,
+        }
     }
-    return code;
+    fn in_string(&self) -> bool {
+        self.in_block || self.in_single || self.in_double
+    }
+    fn update(&mut self, c: char) {
+        if c == '"' {
+            self.in_double = !self.in_double;
+        } else if c == '\'' {
+            self.in_single = !self.in_single;
+        } else if c == '`' {
+            self.in_block = !self.in_block;
+        }
+    }
+}
+
+pub fn remove_comments(code: String) -> String {
+    // removes all comments from source code:
+    // mark uses // for oneline comments, terminated with a newline
+    // mark uses /* */ for multiline comments, terminated with a */
+    // chars inside mark strings (", ', and `) are ignored.
+    let mut comment_oneline = false; // // ...
+    let mut comment_multiline = false; // /* ... */
+    let mut new_code: Vec<String> = Vec::new();
+    let mut string_context = MarkStringContext::new();
+    for line in code.lines() {
+        for (index, c) in line.chars().enumerate() {
+            string_context.update(c);
+            let mut l = line.to_owned();
+            l.push(' ');
+            let mut new_line = String::new();
+            if c == '/' && !string_context.in_string() {
+                let next_char = l.chars().nth(index + 1).unwrap();
+                if next_char == '/' {
+                    // oneline comment opening
+                    comment_oneline = true;
+                } else if next_char == '*' {
+                    // multiline comment opening
+                    comment_multiline = true;
+                }
+            } else if c == '\n' {
+                // terminate oneline comment
+                comment_oneline = false;
+            } else if c == '*' && !string_context.in_string() {
+                let next_char = l.chars().nth(index + 1).unwrap();
+                if next_char == '/' {
+                    // terminate multiline comment
+                    comment_multiline = false;
+                }
+            }
+            if !comment_oneline && !comment_multiline {
+                new_line.push(c);
+            }
+            new_code.push(new_line);
+        }
+    }
+    return new_code.join("");
 }
