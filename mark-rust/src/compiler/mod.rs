ccbbea10f65e50c744fef0a159fc3315ab74acb6
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-02-28 21:38:12 +0300

    commit

--- mark-rust/src/compiler/mod.rs
+++ mark-rust/src/compiler/mod.rs
@@ -1,9 +1,14 @@
 use std::fs;
+use std::path::Path;
 mod remove_comments;
 
 pub fn compile(filename: String) {
-    let mut contents = fs::read_to_string(filename)
-        .expect(crate::errs::throw("Could not read file, does it exist?"));
+    if Path::new(&filename).is_file() {
+        crate::errs::throw("Could not read file, does it exist?");
+    }
+    let c: Result<String, std::io::Error> = fs::read_to_string(filename);
+
+    // TODO:
     contents = remove_comments::remove_comments(contents.clone());
     let content_lines = contents.lines();
     for (_, line) in content_lines.enumerate() {
--- mark-rust/src/main.rs
+++ mark-rust/src/main.rs
@@ -1,6 +1,7 @@
 mod compiler;
 pub mod errs;
 pub mod node;
+pub mod utils;
 
 fn main() {
     compiler::compile(String::from("test.mark"));
