2fcf5c972bf80a09d1f7ad2def6138428d5a849a
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-03-01 14:59:49 +0300

    commit

--- mark-rust/src/compiler/parse_lines.rs
+++ mark-rust/src/compiler/parse_lines.rs
@@ -1,22 +1,10 @@
-struct MarkLineContext {
-    trimmed: Vec<String>,
-    keyword: String
-}
-
-impl MarkLineContext {
-    fn new(line: String) -> MarkLineContext {
-        let trimmed = line.trim().to_string();
-        let separated =  line.split_whitespace().map(|s| s.to_string()).collect();
-        let kw = separated.nth(0).unwrap();
-        MarkLineContext {
-            keyword: trimmed[0].clone(),
-        }
-    }
-
 pub fn parse_lines(code: String) {
     for line in code.lines() {
-        let mut line_whitespace_sep = line.split_whitespace().collect();
-        let line_trimmed =
-        let kw = seperated.nth(0);
+        // trimmed line with no indentation and \n
+        let trimmed = line.trim().to_string();
+        // whitespace-separated line
+        let separated = line.split_whitespace().map(|s| s.to_string()).collect();
+        // keyword, can be a reserved one like require or a node like <div />
+        let kw = separated.nth(0).unwrap();
     }
 }
