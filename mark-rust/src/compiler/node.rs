4e64f2cdbd4d291db7fd96af9aea9a14a2fe290f
baris-inandi <68742481+baris-inandi@users.noreply.github.com>
2022-03-10 00:03:19 +0300

    commit

--- mark-rust/src/compiler/node.rs
+++ mark-rust/src/compiler/node.rs
@@ -18,31 +18,57 @@ pub struct Node {
     pub inner: String,
 }
 
+fn is_string_line(line: String) -> bool {
+    if (line.starts_with("\"") && line.ends_with("\""))
+        || (line.starts_with("'") && line.ends_with("'"))
+    {
+        return true;
+    }
+    return false;
+}
+
+fn rm_first_last(value: &str) -> &str {
+    let mut chars = value.chars();
+    chars.next();
+    chars.next_back();
+    chars.as_str()
+}
+
 impl Display for Node {
     fn fmt(&self, fmt: &mut std::fmt::Formatter) -> std::result::Result<(), std::fmt::Error> {
-        write!(fmt, "[{}:{}] <{}>", self.line_number, self.indent, self.tag)
+        let inner = if self.inner.len() > 0 { "[...]" } else { "" };
+        write!(
+            fmt,
+            "[{}:{}] <{}> {}",
+            self.line_number, self.indent, self.tag, inner
+        )
     }
 }
 
 impl Node {
     pub fn new(code: &str) -> Self {
-        let trimmed = code.trim().to_string();
-        let split: Vec<String> = trimmed.split_whitespace().map(str::to_string).collect();
+        let trimmed = code.trim();
+        let split: Vec<String> = trimmed
+            .to_string()
+            .split_whitespace()
+            .map(str::to_string)
+            .collect();
+        let tag;
+        let mut inner = "";
+        if is_string_line(String::from(trimmed)) {
+            tag = String::from("_document");
+            inner = rm_first_last(trimmed);
+        } else {
+            tag = split[0].clone();
+        }
         let new_node = Node {
             code: String::from(code),
-            tag: split[0].clone(),
+            tag,
             line_number: 0,
             indent: crate::utils::get_indent_level(String::from(code)),
             attrs: String::new(),
-            inner: String::new(),
+            inner: String::from(inner),
         };
         return new_node;
     }
 }
-
-/*
-        // whitespace-separated line
-        let separated: Vec<String> = trimmed.split_whitespace().map(|s| s.to_string()).collect();
-        // keyword, can be a reserved one like require or a node like <div />
-        let kw = separated.get(0).unwrap();
-*/
--- mark-rust/src/compiler/parse_lines.rs
+++ mark-rust/src/compiler/parse_lines.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 pub fn parse_lines(code: String) {
     for line in code.lines() {
         let trimmed = line.trim().to_string();
@@ -5,6 +7,21 @@ pub fn parse_lines(code: String) {
         // skip empty lines
         if trimmed == "" {
             continue;
+        }
+
+        if trimmed.contains("`") {
+            let block_split: Vec<&str> = trimmed.split("`").collect();
+            println!("{}", block_split.len());
+            let block_annotation = block_split[0];
+            let blocks: HashMap<&str, dyn Fn<String, String>> = HashMap::from([
+                ("script", |code| code),
+                ("style", |code| code),
+                ("sass", |code| code),
+                ("scss", |code| code),
+                ("less", |code| code),
+                ("html", |code| code),
+                ("md", |code| code),
+            ]);
         }
 
         let new_node = super::node::Node::new(line);
